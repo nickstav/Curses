c6dbecca41b09ca0245f8e2a03c6f87089890f87
nickstav <nickjblackman@hotmail.com>
2020-10-22 17:00:55 +0100

    rectangle directionality added

--- src/JS/tools/rectangle.js
+++ src/JS/tools/rectangle.js
@@ -59,13 +59,21 @@ function addCorners(cornerSquares, context, gridDimension) {
 
 // use "|" character for the verticals
 function addVerticals(coords, context, gridDimension) {
-    let height = coords.bottomLeft.y - coords.topLeft.y;
+    let height = Math.abs(coords.bottomLeft.y - coords.topLeft.y);
 
     for (let i = 1; i < height; i++) {
-        // A and B correspond to both sides
-        let verticalCoordinate_A = {x: coords.topLeft.x, y: coords.topLeft.y + i};
-        let verticalCoordinate_B = {x: coords.topRight.x, y: coords.topRight.y + i};
-
+        let verticalCoordinate_A, verticalCoordinate_B; // A and B correspond to both sides
+    
+        if (coords.bottomLeft.y > coords.topLeft.y) {
+            // draw vertical downwards
+            verticalCoordinate_A = {x: coords.topLeft.x, y: coords.topLeft.y + i};
+            verticalCoordinate_B = {x: coords.topRight.x, y: coords.topRight.y + i};
+        } else {
+            // draw vertical upwards
+            verticalCoordinate_A = {x: coords.topLeft.x, y: coords.topLeft.y - i};
+            verticalCoordinate_B = {x: coords.topRight.x, y: coords.topRight.y - i};
+        }
+        
         context.fillText('|', verticalCoordinate_A.x * gridDimension.x, verticalCoordinate_A.y * gridDimension.y);
         context.fillText('|', verticalCoordinate_B.x * gridDimension.x, verticalCoordinate_B.y * gridDimension.y);
     }
@@ -73,12 +81,20 @@ function addVerticals(coords, context, gridDimension) {
 
 // use "-" character for the horizontals
 function addHorizontals(coords, context, gridDimension) {
-    let width = coords.topRight.x - coords.topLeft.x;
+    let width = Math.abs(coords.topRight.x - coords.topLeft.x);
 
     for (let i = 1; i < width; i++) {
-        // A and B correspond to top and bottom
-        let horizCoordinate_A = {x: coords.topLeft.x + i, y: coords.topLeft.y};
-        let horizCoordinate_B = {x: coords.bottomLeft.x + i, y: coords.bottomLeft.y};
+        let horizCoordinate_A, horizCoordinate_B; // A and B correspond to top and bottom
+
+        if (coords.topLeft.x < coords.bottomRight.x) {
+            //draw horizontal to the right
+            horizCoordinate_A = {x: coords.topLeft.x + i, y: coords.topLeft.y};
+            horizCoordinate_B = {x: coords.bottomLeft.x + i, y: coords.bottomLeft.y};
+        } else {
+            //draw horizontal to the left
+            horizCoordinate_A = {x: coords.topLeft.x - i, y: coords.topLeft.y};
+            horizCoordinate_B = {x: coords.bottomLeft.x - i, y: coords.bottomLeft.y};
+        }
 
         context.fillText('-', horizCoordinate_A.x * gridDimension.x, horizCoordinate_A.y * gridDimension.y);
         context.fillText('-', horizCoordinate_B.x * gridDimension.x, horizCoordinate_B.y * gridDimension.y);
