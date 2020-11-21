9b350fd92c77bf58b542dc10a6417f6c48403f09
nickstav <nickjblackman@hotmail.com>
2020-11-21 11:54:16 +0000

    overlay height "fix"

--- src/Components/PythonScript.svelte
+++ src/Components/PythonScript.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <div id="overlay" class="w-full h-full fixed top-0 left-0 flex items-center justify-center align-center">
-    <div id="textHolder" class="w-3/4 h-3/4 text-left font-mono text-sm bg-gray-600">
-        <textarea class="w-full h-full px-4 py-4 bg-gray-200">
+    <div id="textHolder" class="h-3/4 w-3/4 text-left font-mono text-sm bg-gray-600 p-4" style="height: 75%">
+        <textarea class="w-full h-full bg-gray-200">
             {$cursesCanvas.pythonScript}
         </textarea>
     </div>
