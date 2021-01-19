d0dd13eaa571d8020814e24e2719872d66a73174
nickstav <nickjblackman@hotmail.com>
2021-01-19 14:03:19 +0000

    toolbar UI update

--- src/Components/Toolbar.svelte
+++ src/Components/Toolbar.svelte
@@ -6,7 +6,7 @@
     import { tools } from '../JS/constants/toolsList.js';
 </script>
 
-<div id="buttons" class="flex flex-col justify-between bg-gray-300 ml-4 mr-4 rounded-md border border-gray-400">
+<div id="buttons" class="flex flex-col h-full justify-start bg-gray-300 mr-4 border-r border-gray-400 pt-6">
     <ToolbarButton label={'line (l)'} editCanvas={()=>{changeTool(tools.LINE)}} img={'./images/buttons/line.png'}/>
     <ToolbarButton label={'rectangle (r)'} editCanvas={()=>{changeTool(tools.RECTANGLE)}} img={'./images/buttons/rectangle.png'}/>
     <ToolbarButton label={'text (t)'} editCanvas={()=>{changeTool(tools.TEXT)}} img={'./images/buttons/text.png'}/>
--- src/Components/ToolbarButton.svelte
+++ src/Components/ToolbarButton.svelte
@@ -14,14 +14,14 @@
 
 </script>
 
-<div style="--bgColour: {bgColour}" class="py-2 px-2">
+<div style="--bgColour: {bgColour}" class="py-4 px-2">
     <button
     id="sidebarButton"
     title={label}
-    class="h-10 w-10 bg-cover text-xs opacity-75 hover:opacity-100 rounded-md"
+    class="flex h-10 w-10 justify-center items-center bg-cover opacity-75 hover:opacity-100 rounded-md"
     style="background-color: var(--bgColour); outline: none"
     on:click={editCanvas} 
     >
-        <img src={img}>
+        <img src={img} class="h-6 w-6">
     </button>
 </div>
