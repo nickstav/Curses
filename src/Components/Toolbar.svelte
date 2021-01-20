4c25e3a9fd1b16564af848fa67b7920d098c256e
nickstav <nickjblackman@hotmail.com>
2021-01-20 11:37:35 +0000

    toolbar reorder and line tool set as default

--- src/Components/Toolbar.svelte
+++ src/Components/Toolbar.svelte
@@ -7,9 +7,9 @@
 </script>
 
 <div id="buttons" class="flex flex-col h-full justify-start bg-theme-secondary mr-4 border-r border-theme-secondary-dark pt-4">
+    <ToolbarButton label={'select (v)'} editCanvas={()=>{changeTool(tools.DRAG)}} img={'./images/buttons/select.png'}/>
     <ToolbarButton label={'line (l)'} editCanvas={()=>{changeTool(tools.LINE)}} img={'./images/buttons/line.png'}/>
     <ToolbarButton label={'rectangle (r)'} editCanvas={()=>{changeTool(tools.RECTANGLE)}} img={'./images/buttons/rectangle.png'}/>
     <ToolbarButton label={'text (t)'} editCanvas={()=>{changeTool(tools.TEXT)}} img={'./images/buttons/text.png'}/>
     <ToolbarButton label={'progress bar (p)'} editCanvas={()=>{changeTool(tools.PROGRESS)}} img={'./images/buttons/progress.png'}/>
-    <ToolbarButton label={'select (v)'} editCanvas={()=>{changeTool(tools.DRAG)}} img={'./images/buttons/select.png'}/>
 </div>
