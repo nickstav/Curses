1b2bfbc95928e0ac92e3c9575e5ce054053b4863
nickstav <nickjblackman@hotmail.com>
2021-01-22 22:42:01 +0000

    secondary colour change

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,9 @@ module.exports = {
         'sidebar': '#f5f5f5',
         'sidebar-lighter': '#ffffff',
         'sidebar-border': '#c2c2c2',
-        'toolbar': '#f9683a',
-        'toolbar-border': '#ff5722',
-        'toolbar-lighter': '#ffbb93',
+        'toolbar': '#bcaaa4', // #f9683a
+        'toolbar-border': '#8c7b75', // '#ff5722'
+        'toolbar-lighter': '#efdcd5', // ffbb93'
         'toolbar-hover': '#ffa270',
         'footer': '#f5f5f5',
         'footer-border': '#c2c2c2'
