6afb04d4b08a8b50b2b29d775469ae3c678e680c
nickstav <nickjblackman@hotmail.com>
2021-01-23 15:01:17 +0000

    Header icons hover states edit

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -9,11 +9,11 @@
     <button
     id="infoButton"
     title="app info"
-    class="h-6 w-6 bg-cover text-xs rounded-md mr-4"
+    class="h-10 w-10 bg-cover text-xs rounded-md mr-4"
     style="outline: none"
     on:click={()=>{cursesCanvas.toggleSplash()}} 
     >
-        <svg xmlns="http://www.w3.org/2000/svg" class="hover:stroke-current" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="#fafafa">
+        <svg xmlns="http://www.w3.org/2000/svg" class=" p-2 hover:p-1" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="#fafafa">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
     </button>
--- src/Components/Sidebar/SidebarTab.svelte
+++ src/Components/Sidebar/SidebarTab.svelte
@@ -29,30 +29,37 @@
     }
 </script>
 
+<style>
+    .selected {
+        pointer-events: none;
+	}
+</style>
 
 <div
 id="sidebarTab" 
 style="--toolsColour: {toolsBGCol}; --exportColour: {exportBGCol}"
 class="w-full h-12 flex flex-row text-xs border-l border-header-border"
 >
-    <button 
+    <button
+    class:selected="{$cursesCanvas.showSidebar === true}"
     aria-label="tools sidebar button"
     style="background-color: var(--toolsColour); outline: none"
     class="flex flex-1 justify-center items-center"
     on:click={showToolsSideBar}
     >
-        <svg xmlns="http://www.w3.org/2000/svg" class="p-3 hover:stroke-current" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke={toolsIconCol}>
+        <svg xmlns="http://www.w3.org/2000/svg" class="p-3 hover:p-2" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke={toolsIconCol}>
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
         </svg>
     </button>
 
     <button 
+    class:selected="{$cursesCanvas.showSidebar === false}"
     aria-label="export sidebar button"
     style="background-color: var(--exportColour); outline: none"
     class="flex flex-1 justify-center items-center" 
     on:click={showExportSideBar}
     >
-        <svg xmlns="http://www.w3.org/2000/svg" class="p-3 hover:stroke-current" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke={exportIconCol}>
+        <svg xmlns="http://www.w3.org/2000/svg" class="p-3 hover:p-2" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke={exportIconCol}>
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16v2a2 2 0 01-2 2H5a2 2 0 01-2-2v-7a2 2 0 012-2h2m3-4H9a2 2 0 00-2 2v7a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-1m-1 4l-3 3m0 0l-3-3m3 3V3" />
         </svg>
     </button>
